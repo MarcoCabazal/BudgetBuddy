d85e203769af6b0b84cc2d60ac008e547110896a
Marco Cabazal <marco.cabazal@chillmill.mobi>
2014-04-28 21:01:52 +0800

    Initial Commit

---
+++ Budget Buddy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Budget Buddy
+//
+//  Created by Marco Cabazal on 4/28/14.
+//  Copyright (c) 2014 The Chill Mill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Budget Buddy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Budget Buddy
+//
+//  Created by Marco Cabazal on 4/28/14.
+//  Copyright (c) 2014 The Chill Mill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
