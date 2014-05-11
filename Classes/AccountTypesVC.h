858e62521be12277832142a3c07044594359503f
Marco Cabazal <marco.cabazal@chillmill.mobi>
2014-05-12 01:29:49 +0800

    Added AccountTypesVC.

---
+++ Classes/AccountTypesVC.h
@@ -0,0 +1,13 @@
+//
+//  AccountTypesVC.h
+//  Budget Buddy
+//
+//  Created by Marco Cabazal on 5/12/14.
+//  Copyright (c) 2014 The Chill Mill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountTypesVC : UITableViewController
+
+@end
