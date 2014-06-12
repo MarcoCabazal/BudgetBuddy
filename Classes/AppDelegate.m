9fd1b39366e659386570157334a779419db86ce1
Marco Cabazal <marco.cabazal@chillmill.mobi>
2014-06-12 10:48:24 +0800

    Included TestFlight init in AppDelegate.

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -13,6 +13,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+    [TestFlight takeOff:@"17976e7c-9163-40c8-8ee8-a2fac2b05a33"];
     [Parse setApplicationId:@"e4URsEi918Rc7lT12pD4ZtiQHH8uPYCDeekxw8uq"
                   clientKey:@"TGvD04hODwV3QXNJPWO1ngRwRq4N4s7nB71rSNV6"];
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
@@ -22,6 +23,9 @@
     
     [[UINavigationBar appearance] setBarTintColor:[UIColor orangeColor]];
     [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+//    [[UITabBar appearance] setBackgroundColor:[UIColor darkGrayColor]];
+    [[UITabBar appearance] setBarTintColor:[UIColor darkGrayColor]];
+    [[UITabBar appearance] setTintColor:[UIColor whiteColor]];
 
 
     self.tabController = [[UITabBarController alloc] init];
